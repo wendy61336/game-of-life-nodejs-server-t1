36e1e6ee1d8bd751b0f8d1aa1d237ed70a6abdcd
WendyTseng <bowlbear10@gmail.com>
2020-03-15 21:20:28 +0800

    heroku port issue

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
   var ws = require('./ws')
 
   var app = express()
-
-  app.listen(3000, function () {
-    
-    console.log('Example app listening on port 3000!')
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, function () {
+    console.log('Example app listening on port '+ PORT)
   })
+
+
+
